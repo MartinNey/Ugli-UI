c04e03fa58ad14559247834923d4321eea2c3f5a
JiahaiHu <892220879@qq.com>
2018-02-06 21:21:07 +0800

    feat: init

---
+++ src/components/react/IndexList/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import styles from '@style/IndexList/index.sass'
+
+class IndexList extends Component {
+  render() {
+    return <div>IndexList</div>
+  }
+}
+
+export default IndexList
